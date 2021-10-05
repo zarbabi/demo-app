7a99397c48f16db06e5209b755815e2310ed2cf7
zarbabi <z.arbabi4@gmail.com>
2021-10-05 21:41:08 +0330

    adding Link Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import AboutUs from "./Pages/AboutUs";
+import AboutUs from "./Pages/AboutUsPage";
 
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About-us Page</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Route path="/" exact={true} component={HomePage} />
       <Route path="/about-us" component={AboutUs} />
     </BrowserRouter>
